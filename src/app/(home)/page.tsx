0638d3ab20706c843734a849b0cb484f87705ec8
KGstievK <kagastiev.kudaibergen.kk@gmail.com>
2024-09-06 21:24:54 +0600

    love

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Home from "@/components/pages/Home";
-import Welcome from "@/components/pages/Welcome/Welcome";
 import React from "react";
 import { usePathname } from "next/navigation";
 
